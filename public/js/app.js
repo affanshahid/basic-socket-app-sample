3e302a2c318ac2be1ab64673e7192fe8d53f57ec
Affan Shahid <affan.shahid.94@gmail.com>
2016-03-24 01:17:09 +0500

    Add front-end form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,27 @@
 /*globals io*/
-var socket = io();
+$(function() {
+    var socket = io();
 
-socket.on('connect', function() {
-    console.log('Connected to socket.io server');
-});
+    socket.on('connect', function() {
+        console.log('Connected to socket.io server');
+    });
 
-socket.on('message', function(message) {
-    console.log('New message:');
-    console.log(message.text);
+    socket.on('message', function(message) {
+        console.log('New message:');
+        console.log(message.text);
+    });
+
+    //Handles submitting of messages
+    var $form = $('#message-form');
+
+    $form.submit(function(event) {
+        event.preventDefault();
+        var $messageField = $form.find('input[name="message"]');
+
+        socket.emit('message', {
+            text: $messageField.val()
+        });
+
+        $messageField.val('').focus();
+    });
 });
