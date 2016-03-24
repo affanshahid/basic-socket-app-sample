1c15b21a6894a3666c96008560dd7d1b7020b318
Affan Shahid <affan.shahid.94@gmail.com>
2016-03-25 00:36:40 +0500

    Add disconnect event and message

--- server.js
+++ server.js
@@ -10,18 +10,31 @@ var io = socketio(server);
 
 app.use(express.static('./public'));
 
+var clientInfo = {};
+
 io.on('connection', function(socket) {
     console.log('User connected via socket.io');
-    
-    var clientInfo;
+
+    socket.on('disconnect', function() {
+        if (clientInfo[socket.id] !== 'undefined') {
+            socket.leave(clientInfo[socket.id].room);
+            io.to(clientInfo[socket.id].room).emit('message', {
+                text: clientInfo[socket.id].name + ' has left the room',
+                name: 'System',
+                timestamp: moment().valueOf()
+            });
+
+            delete clientInfo[socket.id];
+        }
+    });
 
     socket.on('join-room', function(request) {
-        clientInfo = request;
+        clientInfo[socket.id] = request;
 
-        socket.join(clientInfo.room);
-        socket.broadcast.to(clientInfo.room).emit('message', {
+        socket.join(request.room);
+        socket.broadcast.to(request.room).emit('message', {
             name: 'System',
-            text: clientInfo.name + ' has joined!',
+            text: request.name + ' has joined!',
             timestamp: moment().valueOf()
         });
     });
@@ -29,8 +42,8 @@ io.on('connection', function(socket) {
     socket.on('message', function(message) {
         console.log('Message recieved: ' + message.text);
         message.timestamp = moment.now().valueOf();
-        message.name = clientInfo.name;
-        io.to(clientInfo.room).emit('message', message);
+        message.name = clientInfo[socket.id].name;
+        io.to(clientInfo[socket.id].room).emit('message', message);
     });
 
     socket.emit('message', {
